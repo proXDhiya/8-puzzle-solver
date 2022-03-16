ea1024080a7e6bbc79b9f665e0a95b3bad569edf
proXDhiya <prox.dhiya@gmail.com>
2022-03-15 17:48:51 -0700

    update

--- GameLogic/main.c
+++ GameLogic/main.c
@@ -1,11 +1,17 @@
 #include "8puzzle/8puzzle.h"
 
 int main() {
-    // Create array for game
+    // create array
     int array[9];
-    
-    // game loop start
-    game_loop(array);
+
+    // create node list
+    Node node_list[MAX_NODE_LIST_SIZE];
+
+    // game loop
+    game_loop(array, node_list);
+
+    // print node list
+    print_node_list(node_list);
 
     return 0;
 }
